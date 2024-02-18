321a627b8744eb0224e92b8d53e07ee566b7bf2a
Chidi Duru <tchidi4real@gmail.com>
2024-02-18 23:55:18 +0000

    Update on card

--- src/components/common/cards/Card.js
+++ src/components/common/cards/Card.js
@@ -1,6 +1,6 @@
 import style from "./cards.module.css";
-const Card = () => {
-  return <div>stack</div>;
+const Card = ({ data }) => {
+  return <div className={style.card}>{data.title}</div>;
 };
 
 export default Card;
